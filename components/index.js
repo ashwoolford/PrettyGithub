e11d071c8ce26f2be1a7442c2b35d32533e0b51c
ashwoolford <asrafhossain197@gmail.com>
2020-08-11 18:53:48 +0600

    init

---
+++ components/index.js
@@ -0,0 +1,9 @@
+import Head from './Head';
+import UserInfo from './UserInfo';
+import Charts from './Charts';
+import Repos from './Repos';
+import Footer from './Footer';
+import Error from './Error';
+import RateLimit from './RateLimit';
+
+export { Head, UserInfo, Charts, Repos, Footer, Error, RateLimit };
